3aa7508a8e8c17eee48e6c7757c683b4084ebcb1
RenoBisschop <reno.bisschop@icloud.com>
2024-09-26 17:14:41 +0200

    Simple water plane with object floating

---
+++ Assets/Scripts/ObjectProperties.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectProperties : MonoBehaviour
+{
+    public static float waterDensity = 1.0f;
+
+    public float goldDensity = 19.3f;
+    public float ironDensity = 7.9f;
+    public float clayDensity = 2f;
+
+    public float polypropyleneDensity = 0.9f;
+    public float woodDensity = 0.5f;
+    public float bamboo = 0.4f;
+}
